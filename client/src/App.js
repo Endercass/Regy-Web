9b2cfea2461f9371b65ba00724c3d0792c2064e1
endercass <casimirrichardson123@gmail.com>
2023-04-03 02:12:49 -0400

    Ditched debug button

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 import {
-  DiscordMention,
   DiscordMessage,
   DiscordMessages,
 } from "@danktuary/react-discord-message";
+import * as SnowflakeUtils from "discord-snowflake";
 import "./App.scss";
 
 function App() {
@@ -15,21 +14,6 @@ function App() {
       .then((res) => {
         console.log(res);
         setApiResponse(res);
-      });
-  }
-  function addInfraction(data) {
-    fetch("http://localhost:9000/infractions/add", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ infraction: data }),
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        fetchAPI();
       });
   }
   function rmInfraction(i) {
@@ -50,27 +34,6 @@ function App() {
   if (!apiResponse) fetchAPI();
   return (
     <div className="page">
-      <button
-        className="infraction-adder"
-        onClick={() => {
-          addInfraction({
-            regexID: uuidv4(),
-            // message: "regy test phrase",
-            message:
-              "regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase regy test phrase ",
-            messageID: 1091898634762731712,
-            author: {
-              id: 275787354688585730,
-              name: "Endercass",
-              pfpURL:
-                "https://cdn.discordapp.com/avatars/275787354688585730/44c5228ff7252802c17d4fd838845393?size=512",
-            },
-          });
-        }}
-        title={JSON.stringify(apiResponse)}
-      >
-        Add infraction
-      </button>
       <div className="infractions">
         {apiResponse &&
           apiResponse.map((infraction, index, arr) => {
@@ -82,6 +45,12 @@ function App() {
                   <DiscordMessage
                     author={infraction.author.name}
                     avatar={infraction.author.pfpURL}
+                    timestamp={
+                      new Date(
+                        SnowflakeUtils.getTimestamp(`${infraction.messageID}`) *
+                          1000
+                      )
+                    }
                   >
                     {infraction.message}
                   </DiscordMessage>
